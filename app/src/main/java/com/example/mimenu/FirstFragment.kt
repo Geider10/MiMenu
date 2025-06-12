4f30610c6e0ca690f639a0bfb602e86f9b87eb93
Geider Frias <geiderde20@gmail.com>
2025-06-11 21:24:14 -0300

    feat: add navigation component

---
+++ app/src/main/java/com/example/mimenu/FirstFragment.kt
@@ -0,0 +1,27 @@
+package com.example.mimenu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.mimenu.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding : FragmentFirstBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.btnOne.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+        return binding.root
+    }
+
+}
