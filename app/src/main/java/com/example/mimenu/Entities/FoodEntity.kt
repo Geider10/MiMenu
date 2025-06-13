039a87acfc473cbdb6089766c8e08fbe78f5f1fd
Geider Frias <geiderde20@gmail.com>
2025-06-13 19:20:35 -0300

    create food mockeada to BuyFragment

---
+++ app/src/main/java/com/example/mimenu/Entities/FoodEntity.kt
@@ -0,0 +1,9 @@
+package com.example.mimenu.Entities
+
+data class FoodEntity (
+    val id : Int,
+    val name : String,
+    val price : Int,
+    val description : String,
+    val category : String
+        )
--- app/src/main/java/com/example/mimenu/SecondFragment.kt
+++ app/src/main/java/com/example/mimenu/SecondFragment.kt
@@ -5,55 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mimenu.Entities.FoodEntity
+import com.example.mimenu.databinding.FragmentSecondBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentSecondBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
+
+    private fun createFoodMock() : List<FoodEntity>{
+        var foodList : List<FoodEntity> = listOf(
+            FoodEntity(1, "Doble Cuarto Xl" ,119000, "Mega hamburguesa", "Hamburguesa"),
+            FoodEntity(2, "Epica Huevo" ,113000, "Mega hamburguesa", "Hamburguesa"),
+            FoodEntity(3, "Mega Doble Big Bang" ,139000, "Mega hamburguesa", "Hamburguesa"),
+            FoodEntity(4, "Sundae de Chocalate" ,2900, "Helado de crema americana", "Sundae"),
+            FoodEntity(5, "Sundae de Frutilla" ,2900, "Helado de crema americana", "Sundae"),
+            FoodEntity(6, "Sundae de Dulce de Leche" ,2900, "Helado de crema americana", "Sundae"),
+            FoodEntity(7, "Café con 2 Medialunas" ,2399, "2 medialunas de manteca acompañadas de un café" , "Team Dulce"),
+            FoodEntity(7, "Medialuna" ,1000, "medialunas de manteca" , "Team Dulce"),
+            FoodEntity(7, "Café con 2 Medialunas de jamón y queso" ,2399, "2 medialunas y un café" , "Team Dulce"),
+            )
+
+        return  foodList
+    }
+
 }
