041294029e9f42cf75091edd88fd0fe2d1e01cbd
Geider Frias <geiderde20@gmail.com>
2025-06-18 23:56:18 -0300

    add cartAdapter and render order mock in cartFragment

---
+++ app/src/main/java/com/example/mimenu/ui/cart/CartAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.mimenu.ui.cart
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mimenu.Entities.OrderEntity
+import com.example.mimenu.databinding.OrderItemCartBinding
+
+class CartAdapter(private val listOrder : List<OrderEntity>) : RecyclerView.Adapter<CartAdapter.CartViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = OrderItemCartBinding.inflate(inflater,parent, false)
+        return CartViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        val orderItem = listOrder.get(position)
+        holder.setValues(orderItem)
+    }
+
+    override fun getItemCount() : Int = listOrder.size
+
+    inner class CartViewHolder(private val binding : OrderItemCartBinding) : RecyclerView.ViewHolder(binding.root){
+        fun setValues(order : OrderEntity){
+            binding.imgItemCart.setImageResource(order.img)
+            binding.tvNameItemCart.text = order.name
+            binding.tvQuantityItemCart.text = order.quantity.toString()
+            binding.tvPriceItemCart.text ="$ " + order.priceTotal.toString()
+        }
+    }
+
+}
--- app/src/main/java/com/example/mimenu/ui/cart/CartFragment.kt
+++ app/src/main/java/com/example/mimenu/ui/cart/CartFragment.kt
@@ -5,20 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mimenu.Entities.FoodEntity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mimenu.Entities.OrderEntity
 import com.example.mimenu.R
 import com.example.mimenu.databinding.FragmentCartBinding
 
 
 class CartFragment : Fragment() {
+
     private lateinit var binding: FragmentCartBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCartBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadRecyclerView()
+    }
+    private fun loadRecyclerView(){
+        val adapter = CartAdapter(setOrderMockeados())
+        binding.rvCart.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvCart.adapter = adapter
     }
 
     private fun setOrderMockeados(): List<OrderEntity>{
