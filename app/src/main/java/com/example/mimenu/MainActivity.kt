7caa203290b853700924c9c2322af805c9397707
Geider Frias <geiderde20@gmail.com>
2025-06-24 20:42:26 -0300

    feat: create toolbar custom and add back arrow in toolbar

--- app/src/main/java/com/example/mimenu/MainActivity.kt
+++ app/src/main/java/com/example/mimenu/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.example.mimenu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.fragment.app.commit
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.example.mimenu.R
 import com.example.mimenu.databinding.ActivityMainBinding
-import com.example.mimenu.ui.buy.SecondFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var bottomNav : BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +20,15 @@ class MainActivity : AppCompatActivity(){
         setContentView(binding.root)
 
         bottomNav = binding.bottomNavigationView
-        val navController = findNavController(R.id.fragmentContainerView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val barConfig = AppBarConfiguration(setOf(
+            R.id.firstFragment, R.id.secondFragment, R.id.cartFragment
+        ))
+
+
+        NavigationUI.setupWithNavController(binding.toolbar,navController,barConfig)
         bottomNav.setupWithNavController(navController)
     }
 }
