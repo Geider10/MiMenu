add6039b74479c26c0250f21663c8a16137729ad
Geider Frias <geiderde20@gmail.com>
2025-06-28 22:06:27 -0300

    feat: rendering categories in buyFragment using rv horizontal

---
+++ app/src/main/java/com/example/mimenu/data/Entities/CategoryEntity.kt
@@ -0,0 +1,6 @@
+package com.example.mimenu.data.Entities
+
+data class CategoryEntity (
+    val id : Int,
+    val name : String
+        )
--- app/src/main/java/com/example/mimenu/ui/buy/SecondFragment.kt
+++ app/src/main/java/com/example/mimenu/ui/buy/SecondFragment.kt
@@ -11,6 +11,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mimenu.data.Entities.FoodEntity
 import com.example.mimenu.R
+import com.example.mimenu.data.Entities.CategoryEntity
 import com.example.mimenu.data.Entities.OrderEntity
 import com.example.mimenu.databinding.FragmentSecondBinding
 class SecondFragment : Fragment(), OnFoodClick {
@@ -29,14 +30,15 @@ class SecondFragment : Fragment(), OnFoodClick {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loadRecyclerView()
+        setupRecyclerFood()
+        setupRecyclerCategory()
     }
-    private fun loadRecyclerView(){
-        var adapter = BuyAdapter(createFoodMock(),this)
-        binding.rvBuy.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvBuy.adapter = adapter
+    private fun setupRecyclerFood(){
+        var adapter = BuyAdapter(getFoodMock(),this)
+        binding.rvFoodBuy.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvFoodBuy.adapter = adapter
     }
-    private fun createFoodMock() : List<FoodEntity>{
+    private fun getFoodMock() : List<FoodEntity>{
         val foodList : List<FoodEntity> = listOf(
             FoodEntity(1, "Doble Cuarto Xl" ,11900, "Mega hamburguesa de doble carne, doble queso cheddar, cebollita, kétchup y mostaza en pan de papa.", "Hamburguesa", R.mipmap.hamburguer),
             FoodEntity(2, "Epica Huevo" ,11300, "Mega hamburguesa de carne, queso cheddar, panceta, huevo, cebolla grillada y barbacoa ahumada en pan de papa.", "Hamburguesa", R.mipmap.hamburguer),
@@ -56,5 +58,21 @@ class SecondFragment : Fragment(), OnFoodClick {
         val order = OrderEntity(name = food.name, description = food.description, price = food.price, img = food.img, priceTotal = food.price, quantity = 1)
         val action = SecondFragmentDirections.actionSecondFragmentToFoodDetailFragment(order)
         findNavController().navigate(action)
+    }
+
+    private fun setupRecyclerCategory(){
+        var adapter = CategoryAdapter(getCategoryMock())
+        binding.rvCategoryBuy.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvCategoryBuy.adapter = adapter
+    }
+    private fun getCategoryMock(): List<CategoryEntity> {
+        return listOf(
+            CategoryEntity(1, "Hamburguesa"),
+            CategoryEntity(2, "Sundae"),
+            CategoryEntity(3, "Team Dulce"),
+            CategoryEntity(4, "Ensaladas"),
+            CategoryEntity(5, "Nuggets"),
+            CategoryEntity(6, "bebidas"),
+        )
     }
 }
